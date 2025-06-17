6796dc2de1a16f492262545e6333227aac89c841
saves <diodemusicuk@gmail.com>
2025-06-17 02:46:10 +0100

    Refactor initialization methods and rename init_list to _init_list for consistency

--- src/chip.py
+++ src/chip.py
@@ -4,28 +4,28 @@ import pyglet
 class Chip(pyglet.window.Window):
     def __init__(self) -> None:
         self.clear()
-        self.key_inputs = self.init_list(num=16)
-        self.display_buffer = self.init_list(num=32 * 64)
-        self.memory = self.init_list(num=4096)
-        self.gpio = self.init_list(16)
+        self.key_inputs = self._init_list(num=16)
+        self.display_buffer = self._init_list(num=32 * 64)
+        self.memory = self._init_list(num=4096)
+        self.gpio = self._init_list(16)
         self.sound_timer = 0
-        self.delay_time = 0
+        self.delay_timer = 0
         self.index = 0
         self.pc = 0
         self.stack = []
         self.fonts = []
 
-    def initialize(self):
+    def initialize(self) -> None:
         self.clear()
-        self.memory = self.init_list(num=4096)
-        self.gpio = self.init_list(16)
-        self.display_buffer = self.init_list(num=32 * 64)
+        self.memory = self._init_list(num=4096)
+        self.gpio = self._init_list(16)
+        self.display_buffer = self._init_list(num=32 * 64)
         self.stack = []
-        self.key_inputs = self.init_list(num=16)
+        self.key_inputs = self._init_list(num=16)
         self.opcode = 0
         self.index = 0
         self.sound_timer = 0
-        self.delay_time = 0
+        self.delay_timer = 0
         self.should_draw = False
         self.pc = 0x200
 
@@ -35,7 +35,7 @@ class Chip(pyglet.window.Window):
             self.memory[i] = self.fonts[i]
             i += 1
 
-    def init_list(self, num: int) -> list[int]:
+    def _init_list(self, num: int) -> list[int]:
         return [0] * num
 
     def load_rom(self, rom_file_name: str) -> None:
@@ -51,7 +51,29 @@ class Chip(pyglet.window.Window):
             self.memory[i + offset] = binary[i]
             i += 1
 
-        print(self.memory)
+    def cycle(self) -> None:
+        self.opcode = self.memory[self.pc]
+        self.vx = (self.opcode & 0x0F00) >> 8
+        self.vy = (self.opcode & 0x00F0) >> 4
+        self.pc += 2
+        extracted_op = self.opcode & 0xF00
 
-    def get_fonts(self):
-        pass
+        try:
+            self.funcmap[extracted_op]()
+        except Exception as e:
+            print(f"Unknown instructions: {self.opcode}: {e}")
+
+        if not self.delay_timer > 0:
+            return
+
+        self.delay_timer -= 1
+
+        if not self.sound_timer > 0:
+            return
+
+        self.sound_timer -= 1
+
+        if not self.sound_timer == 0:
+            return
+
+        # TODO: play sound
