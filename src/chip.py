219674a747a82549eb8227a53e6220e68dcfcff7
saves <diodemusicuk@gmail.com>
2025-06-17 02:15:02 +0100

    Refactor Chip class initialization and add clear method

--- src/chip.py
+++ src/chip.py
@@ -3,6 +3,7 @@ import pyglet
 
 class Chip(pyglet.window.Window):
     def __init__(self) -> None:
+        self.clear()
         self.key_inputs = self.init_list(num=16)
         self.display_buffer = self.init_list(num=32 * 64)
         self.memory = self.init_list(num=4096)
@@ -12,6 +13,36 @@ class Chip(pyglet.window.Window):
         self.index = 0
         self.pc = 0
         self.stack = []
+        self.fonts = []
+
+    def initialize(self):
+        self.clear()
+        self.memory = self.init_list(num=4096)
+        self.gpio = self.init_list(16)
+        self.display_buffer = self.init_list(num=32 * 64)
+        self.stack = []
+        self.key_inputs = self.init_list(num=16)
+        self.opcode = 0
+        self.index = 0
+        self.sound_timer = 0
+        self.delay_time = 0
+        self.should_draw = False
+        self.pc = 0x200
+
+        i = 0
+
+        while i < 80:
+            self.memory[i] = self.fonts[i]
+            i += 1
+
+    def clear(self):
+        pass
 
     def init_list(self, num: int) -> list[int]:
         return [0] * num
+
+    def load_rom(self, rom):
+        pass
+
+    def get_fonts(self):
+        pass
